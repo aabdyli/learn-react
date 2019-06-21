cf7199170af6e2eb2770d32d58a0b0f3d4fa692c
Albi Abdyli <albi.abdyli@outlook.com>
2019-06-21 23:49:09 +0200

    updated dependencies

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -40,8 +40,8 @@ function RepoGrid (props) {
                   alt={'Avarat for ' + repo.owner.login}
                   className='avatar' />
               </li>
-              <li><a href={repo.html_url} target='_blank'>{repo.name}</a></li>
-              <li><a href={repo.owner.html_url} target='_blank'>@{repo.owner.login}</a></li>
+              <li><a href={repo.html_url} target='_blank' rel="noopener noreferrer">{repo.name}</a></li>
+              <li><a href={repo.owner.html_url} target='_blank' rel="noopener noreferrer">@{repo.owner.login}</a></li>
               <li>{repo.stargazers_count}</li>
             </ul>
           </li>
